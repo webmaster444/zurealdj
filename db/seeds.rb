b6388b9e862626602a381e8a9141624743290208
webmaster444 <jlmobile710@gmail.com>
2017-01-05 11:10:04 +0200

    Organizer step 1 title fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 puts "Create users ..."
 
-User.create email: 'admin@gmail.com', password: 'secret', password_confirmation: 'secret', confirmed: true, role_id: Role.admin.id
-User.create email: 'dj@gmail.com', password: 'secret', password_confirmation: 'secret', confirmed: true, role_id: Role.dj.id
-User.create email: 'organizer@gmail.com', password: 'secret', password_confirmation: 'secret', confirmed: true, role_id: Role.organizer.id
+User.create email: 'admin1@gmail.com', name: 'admin1', password: 'secret', password_confirmation: 'secret', confirmed: true, role_id: Role.admin.id
+User.create email: 'admin2@gmail.com', name: 'admin2', password: 'secret', password_confirmation: 'secret', confirmed: true, role_id: Role.admin.id
+User.create email: 'dj@gmail.com', name: 'dj1', password: 'secret', password_confirmation: 'secret', confirmed: true, role_id: Role.dj.id
+User.create email: 'dj2@gmail.com', name: 'dj2', password: 'secret', password_confirmation: 'secret', confirmed: true, role_id: Role.dj.id
+User.create email: 'dj3@gmail.com', name: 'dj3', password: 'secret', password_confirmation: 'secret', confirmed: true, role_id: Role.dj.id
+User.create email: 'dj4@gmail.com', name: 'dj4', password: 'secret', password_confirmation: 'secret', confirmed: true, role_id: Role.dj.id
+User.create email: 'dj5@gmail.com', name: 'dj5', password: 'secret', password_confirmation: 'secret', confirmed: true, role_id: Role.dj.id
+User.create email: 'organizer@gmail.com', name: 'org1', password: 'secret', password_confirmation: 'secret', confirmed: true, role_id: Role.organizer.id
