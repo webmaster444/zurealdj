92b2b6ca3a1358d32baf0ed8bdd59900240a8950
webmaster444 <jlmobile710@gmail.com>
2017-03-24 11:47:21 +0200

    instagram feed 137, UI 299 inbox badge fix

--- config/routes.rb
+++ config/routes.rb
@@ -166,6 +166,7 @@ Rails.application.routes.draw do
     resources :notifications, only: [:index, :update]
     resources :slides, only: [:index]
     resources :top_rated_djs, only: [:index]
+    resources :instagram, only: [:index]
     resources :subscriptions, only: [:index]
   end
 end
