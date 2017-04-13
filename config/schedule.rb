be32d38f3cf1c713ab7efd6a82a050b3b9443e7d
webmaster444 <jlmobile710@gmail.com>
2017-04-13 15:06:40 +0300

    Configure scheduler logs.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,6 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# set :output, "/path/to/my/cron_log.log"
-
+set :environment, 'development'
+set :output, 'log/cron.log'
 
 every 30.minutes do
-
   runner "SubscriptionWorker.perform_async()"
-
 end
