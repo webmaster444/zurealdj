79ffab5d446210c75054bf9d1f3c7f9162134cb1
webmaster444 <jlmobile710@gmail.com>
2017-08-31 21:37:55 +1200

    aws setting

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,7 +5,7 @@ Paperclip::Attachment.default_options.merge!(
   storage:              :s3,
   s3_credentials:       Rails.configuration.aws,
   #s3_permissions:       :private,
-  s3_protocol:          'https',
+  # s3_protocol:          'https',
   s3_region:            'ap-southeast-2',
   s3_host_name:         's3-ap-southeast-2.amazonaws.com'
 )
