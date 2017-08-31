3d4585f3299d46d81645725a48a33a7e52e17123
webmaster444 <jlmobile710@gmail.com>
2017-09-01 04:49:27 +1200

    remove node_modules

--- node_modules/animate.scss/lib/animate-scss.rb
+++
@@ -1,9 +0,0 @@
-require 'animate/version'
-
-module Animate
-  if defined?(Rails)
-    require 'animate/engine'
-  else
-    Sass.load_paths << File.expand_path("../../vendor/assets/stylesheets", __FILE__)
-  end
-end
--- node_modules/animate.scss/lib/animate/engine.rb
+++
@@ -1,4 +0,0 @@
-module Animate
-  class Engine < ::Rails::Engine
-  end
-end
