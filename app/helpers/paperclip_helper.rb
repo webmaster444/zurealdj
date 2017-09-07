45893c9ae2afda5b71178c80f67f544b5004482d
webmaster444 <jlmobile710@gmail.com>
2017-09-08 03:07:50 +1200

    add scaled image to view

--- app/helpers/paperclip_helper.rb
+++ app/helpers/paperclip_helper.rb
@@ -2,8 +2,8 @@ module PaperclipHelper
   def paperclip_url(paperclip_attachment, style = :original)
     if Paperclip::Attachment.default_options[:storage] == :azure
       paperclip_attachment.exists? ? paperclip_attachment.try(:azure_uri, style) : nil
-    elsif Paperclip::Attachment.default_options[:storage] == :s3
-      paperclip_attachment.exists? ? paperclip_attachment.try(:url, style) : nil
+    elsif Paperclip::Attachment.default_options[:storage] == :s3 && paperclip_attachment.exists?
+      paperclip_attachment.try(:url, style)
     else
       request.base_url + paperclip_attachment.try(:url, style)
     end
--- app/views/admin/equipments/index.json.jbuilder
+++ app/views/admin/equipments/index.json.jbuilder
@@ -3,6 +3,6 @@ json.equipments @equipments.each do |equipment|
   json.created_at time_ago_in_words(equipment.created_at) + ' ' + t('datetime.ago') + ' ' + t('datetime.at') + ' ' + equipment.created_at.strftime("%H:%M")
 
   json.title equipment.title
-  json.icon equipment.icon.url
+  json.icon paperclip_url(equipment.icon, :medium)
 end
 json.count @count
